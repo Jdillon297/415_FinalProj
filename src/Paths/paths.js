a8c2d82eca783c7d68693d9825259884dc368dc1
Jacob Dillon <jacobdillon119@gmail.com>
2024-03-23 02:07:07 -0700

    inital commit and cosmetics for the site

---
+++ src/Paths/paths.js
@@ -0,0 +1,6 @@
+const paths = {
+  login: "../public/Login",
+  register: "../public/Register",
+  topics: "../public/Topics",
+};
+module.exports = paths;
---
+++ src/router.js
@@ -0,0 +1,10 @@
+const path = require("path");
+const routes = require("./Paths/paths");
+
+const router = {
+  loginPath: path.join(__dirname, routes.login),
+  registerPath: path.join(__dirname, routes.register),
+  topicsPath: path.join(__dirname, routes.topics),
+};
+
+module.exports = router;
